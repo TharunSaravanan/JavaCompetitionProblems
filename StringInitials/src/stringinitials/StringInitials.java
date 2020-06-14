8d13545c1d15855bbb1497d90a47846e05c63861
TharunSaravanan <bstharun@gmail.com>
2020-06-14 17:59:43 -0500

    Java Word initials

---
+++ StringInitials/src/stringinitials/StringInitials.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringinitials;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tharun
+ */
+public class StringInitials {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter number of phrases");
+        int length = in.nextInt();
+
+        String[] words = new String[length];
+        
+        System.out.println("Enter " + length + " words. ex. kit-kat");
+
+        for (int j = 0; j < words.length; j++) {
+            words[j] = in.next();
+        }
+
+        for (int k = 0; k < words.length; k++) {
+
+            String currentWord = words[k];
+            
+            for (int i = 0; i < currentWord.length(); i++) {
+                char currentChar = currentWord.charAt(i);
+
+                if (i == 0) {
+                    System.out.print(Character.toUpperCase(currentChar));
+                }
+
+                if (currentChar == '-') {
+                    System.out.print(Character.toUpperCase(currentWord.charAt(i + 1)));
+                }
+            }
+            
+            System.out.println();
+
+        }
+    }
+
+}
