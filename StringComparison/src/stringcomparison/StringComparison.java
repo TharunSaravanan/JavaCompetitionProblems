768cf2b96524694b02c761587c390fa0f3b71691
TharunSaravanan <bstharun@gmail.com>
2020-07-07 19:40:57 -0500

    String comparison

--- StringComparison/src/stringcomparison/StringComparison.java
+++ StringComparison/src/stringcomparison/StringComparison.java
@@ -1,13 +1,7 @@
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package stringcomparison;
 
 /**
- *
+ * Compares two strings and return ASCII character difference of first different characters
  * @author tharun
  */
 public class StringComparison {
